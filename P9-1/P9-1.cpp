4c4e6b8276ed09ad53c2b333d8abe0b300db446f
Atherton Bunner <a-bunner@onu.edu>
2026-04-02 08:44:04 -0400

    Completed P9-1

--- P9-1/P9-1.cpp
+++ P9-1/P9-1.cpp
@@ -5,9 +5,9 @@ using namespace std;
 // Atherton BUnner; hip, hip, array!
 
 int main() {
-	srand(time(0));
-	const int ARRAYSIZE = 10;
-	int randomint[ARRAYSIZE];
+	srand(time(0)); // required for rand() fucntion
+	const int ARRAYSIZE = 10; // constant to allow extending the array
+	int randomint[ARRAYSIZE]; // array
 	// first bullet point
 	cout << "The random integers: ";
 	for (int i = 0; i < ARRAYSIZE; i++) {
