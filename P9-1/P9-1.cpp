08dad1756a23d944792683b5d33ab06eb17ce6c2
Atherton Bunner <a-bunner@onu.edu>
2026-04-02 08:34:28 -0400

    Every even element

--- P9-1/P9-1.cpp
+++ P9-1/P9-1.cpp
@@ -11,12 +11,19 @@ int main() {
 	// first bullet point
 	cout << "The random integers: ";
 	for (int i = 0; i < ARRAYSIZE; i++) {
-		randomint[i] = 1000 + rand() % 9000;
+		randomint[i] = 1000 + rand() % 9000; // makes 4 digit random number
 		cout << randomint[i] << " ";
 	}
 	// second bullet point
 	cout << endl << "Even indices:	     ";
 	for (int j = 0; j < ARRAYSIZE; j = j + 2) { // makes the even numbers appear
 		cout << randomint[j] << " ";
+	}
+	// third bullet point
+	cout << endl << "Even values:         ";
+	for (int k = 0; k < ARRAYSIZE; k++) {
+		if (randomint[k] % 2 == 0) { // checks if even and outputs number if true
+			cout << randomint[k] << " ";
+		}
 	}
 }
